336e907d20e53b421367591a75aee2adb75d5da8
eldertek <andrelauret@eclipse-technology.eu>
2025-03-21 16:02:33 +0100

    update settings.py

--- backend/pg_pointage/settings.py
+++ backend/pg_pointage/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', 'pg_pointage'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
+        'USER': os.getenv('DB_USER', 'pguser'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'pguser'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
